9df32ff392838ec36020aab60b306a230f17d8bd
Matthew Perez <matthew.j.perez96@gmail.com>
2019-10-04 18:45:57 -0400

    finalized version of rmd r and pdf file submitted to the professor
    finished program for final submission
    modified and corrected comments from previous verisons
    
    Shakilla provided responces to quesitons not answered in previous versions
    
    Co-Authored-By: hoqueshakila <hoqueshakila@users.noreply.github.com>

--- Group_Home_Work_02/Bob_Group_Chapter_3_Questions_8-9.R
+++ Group_Home_Work_02/Bob_Group_Chapter_3_Questions_8-9.R
@@ -9,76 +9,137 @@
 rm(list = ls())
 dev.off()
 
-# IMPORT LIBRARIES FOR REGRESSION
-
-
 # IMPORT AND MODIFY/CLEAN DATA
-file_path = "../Data_Sources/Auto.data"
-data = read.table(file_path)
-
-# SET CATAGORICAL DATA
-
-#8a
-#previuos data cleanup
-rm(list = ls())
-dev.off()
-#read in table
-Auto=read.table("Auto.data")
-Auto=read.table("Auto.data",header=T,na.string="?")
-
-#cheking for missing data
+web = "http://faculty.marshall.usc.edu/gareth-james/ISL/Auto.data"
+Auto=read.table(web,header=T,na.string="?")
+dim(Auto)
 colSums(is.na(Auto))
 Auto=na.omit(Auto)
-fix(Auto)
 dim(Auto)
-is.na(Auto)
 attach(Auto)
-lm.fit=lm(mpg~horsepower)
+
+# 8. This question involves the use of simple linear regression on the 
+# Auto data set.
+# 8. (a) Use the lm() function to perform a simple linear regression 
+# with mpg as the response and horsepower as the predictor. Use the summary() 
+# function to print the results. Comment on the output.
+lm.fit = lm(Auto$mpg~Auto$horsepower)
 lm.fit
 summary(lm.fit)
 
-cor(horsepower,mpg)
-#8 i:  yes, there is a connection between horsepower and mpg
-#8 ii: correlation is strong : -.78
-#8 iii: correlation is negative
-predict(lm.fit,data.frame(horsepower=98),interval="confidence")
-predict(lm.fit,data.frame(horsepower=98),interval="prediction")
+# 8. i. Is there a relationship between the predictor and the response?
+#### Comments
+# Yes, there is a connection between horsepower and mpg
 
-#8iv: predicted mpg associated with horsepower of 98 is 24.47
-# associated 95% confidence interwals is (23.97,24.96)
-#associated 95% prediction interwals is (14.8, 34.12)
-#8b
-abline(lm.fit,col="red",lwd=4)
-#8c
-plot(horsepower,mpg, pch=1, col="darkgreen")
-#comments: ??????
+# 8. ii. How strong is the relationship between the predictor and the response?
+#### Comments
+# The correlation is strong: -0.78
 
+# 8. iii. Is the relationship between the predictor and the response positive 
+# or negative?
+#### Comments
+# The correlation is negative
+
+# 8. iv. What is the predicted mpg associated with a horsepower of 98? 
+# What are the associated 95 % confidence and prediction intervals?
+predict(lm(mpg~horsepower),data.frame(horsepower=98),interval="confidence")
+predict(lm(mpg~horsepower),data.frame(horsepower=98),interval="prediction")
+
+# 8. (b) Plot the response and the predictor. 
+# Use the abline() function to display the least squares regression line.
+
+plot(Auto$horsepower,Auto$mpg, pch=1, col="darkgreen")
+abline(lm.fit, col="red", lwd=4) 
+# 8. (c) Use the plot() function to produce diagnostic plots of the 
+#least squares regression fit. Comment on any problems you see with the fit.
+
+par(mfrow=c(2,2))
+plot(lm.fit)
+
+#### Comments:
+# In the residual vs. fitted model, the plots show a U shape pattern, 
+# that is violating the linearity. In the second model, The normal Q-Q, 
+# the plot shows there is a linear relationship. The 3rd model represents 
+# the variance of the error stayed the same and the 4th model showed there were 
+# few outliers and leverage but the fitted line violated linearity
 
 
 # 9: MULTIPLE REGRESSION 
 #9a produce scatterplot of all the data
 names(Auto)
-as.factor(cylinders)
-as.factor(year)
-as.factor(origin)
-pairs(Auto)
+pairs( Auto, lower.panel = NULL)
+
 #9b matrix of correlations
-Auto=Auto[,-9]
-cor(Auto)
+cor(Auto[,-9])
+
 #9c multiple liniar regression with mpg as response variable
-fit=lm(mpg~cylinders+displacement+horsepower+weight+acceleration+year,origin)
+fit=lm(Auto$mpg~
+         Auto$cylinders+
+         Auto$displacement+
+         Auto$horsepower+
+         Auto$weight+
+         Auto$acceleration+
+         Auto$year+
+         Auto$origin
+)
 summary(fit)
 coefficients(fit)
-#notes here->
+
+# 9. i. Is there a relationship between the predictors and the response?
+#### Comments
+# Yes, there is a relationship between the predictor and the response, 
+# we confirmed it by looking at the H null hypothesis. The p-value corresponds 
+# the f stats at 2.037
+
+# 9. ii. Which predictors appear to have a statistically significant 
+# relationship to the response?
+
+#### Comments
+# All the predictors are statistically significant, excluding “cylinder”, 
+# “horsepower” and “acceleration”  because  p-value associated each predictor’s 
+# t-statistics 
+
+# 9. iii. What does the coefficient for the year variable suggest?
+
+#### Comments
+# The coefficient for the year suggests that as we increase the year by 
+# 1 the fuel-efficient also increases by 0.750 while the other predictor 
+# value stays the same. For example, the car became more fuel-efficient every 
+# year.
 
 #9d produce diagnostic plots
 par(mfrow=c(2,2))
 plot(fit)
-#9e use * and : to add interactions effects
-fitNew=lm(mpg~year*acceleration)
-summary(fitNew)
 
-#9f transformatin of varibles
-fit=lm(mpg~cylinders+displacement+horsepower+I(weight^2)+I(acceleration^2))
+#### Comments
+# The residual vs. fitted model indicated a curvier line and that violates 
+# the linearity. The Q-Q plot does satisfy the linearity because of having 
+# a straight fitted line with a positive strong correlation between standard 
+# residual and theoretical quantiles. The scale location model shows a lot of 
+# unusual outliers in the model but the standard residual vs leverage have shown 
+# fewer outliers and have a high leverage point in the model.
+
+
+#9e use * and : to add interactions effects
+fitNew=lm(Auto$mpg~Auto$year*Auto$acceleration)
 summary(fitNew)
-# summary->       
+par(mfrow=c(2,2))
+plot(fitNew)
+
+#### Comments
+# When we look at the p-value the association between the displacement and 
+# the weight seems to be more statistically significant, however, the 
+# association between the cylinder and displacement does not look statistically 
+# significant. 
+
+## 9. (f) Try a few different transformations of the variables, such as log(X), 
+# √X, X2. Comment on your findings.
+plot( log(horsepower), mpg, abline(lm( mpg~log(horsepower))))
+plot( sqrt(horsepower), mpg, abline(lm(mpg~sqrt(horsepower))))
+plot( (horsepower)^2, mpg, abline(lm(mpg~I(horsepower^2))))
+
+
+#### Comments
+# When we used “horsepower” as our single predictor and to try different 
+# transformation, we noticed only the log transformation gave us the most 
+# liner looking plot for the “horsepower” 	
