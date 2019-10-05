ee3993685c272f231ef0d0c47e9344d321535cdb
Elaishalev <elaish24@gmail.com>
2019-10-05 19:30:45 -0400

    Initial commit for start of home work 3
    Set up template for both R and Rmd files to answer home work questions.

---
+++ Group_Home_Work_03/Bob_Group_Chapter_4_Question_11.R
@@ -0,0 +1,26 @@
+# HOMEWORK #2
+# GROUP: Bob 
+# Maryla Wozniak
+# Shakila Hoque
+# Elai Shalev 
+# Matthew Perez
+
+# GENERAL SETUP
+rm(list = ls())
+dev.off()
+
+# IMPORT CLEAN DATA
+
+# install.packages("ISLR")
+library("ISLR")
+Auto_local = Auto
+attach(Auto_local)
+
+# Question 11: Develop a model to predict whether a car has high/low mpg.
+# (a) 
+# (b)
+# (c)
+# (d)
+# (e)
+# (f)
+# (g)??? OPTIONAL!
